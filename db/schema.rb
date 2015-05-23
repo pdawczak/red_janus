8d3ca5b802d9243107363f85a172b7a1eeb9ebb2
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-05-23 15:52:46 +0100

    Add attributes to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521160512) do
+ActiveRecord::Schema.define(version: 20150523144333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 20150521160512) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title",                               null: false
+    t.string   "first_names",                         null: false
+    t.string   "middle_names"
+    t.string   "last_names",                          null: false
+    t.date     "dob",                                 null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,19 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def user
-    @user ||= users(:pdawczak)
+    @user ||= users(:joe)
   end
 
   def test_valid
     assert user.valid?
+  end
+
+  def test_user_attributes
+    assert_respond_to user, :email
+    assert_respond_to user, :title
+    assert_respond_to user, :first_names
+    assert_respond_to user, :middle_names
+    assert_respond_to user, :last_names
+    assert_respond_to user, :dob
   end
 end
