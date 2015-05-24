d8b259b30d2a78069763be3adab262db014b9b81
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-05-24 20:28:56 +0100

    Update User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,4 +17,24 @@ class UserTest < ActiveSupport::TestCase
     assert_respond_to user, :last_names
     assert_respond_to user, :dob
   end
+
+  def test_legacy_new
+    user = User.new(firstNames: "Joe",
+                    middleNames: "von",
+                    lastNames: "Tester")
+
+    assert_equal "Joe",    user.first_names
+    assert_equal "von",    user.middle_names
+    assert_equal "Tester", user.last_names
+  end
+
+  def test_legacy_update
+    user.update(firstNames:  "Updated FirstNames",
+                middleNames: "Updated MiddleNames",
+                lastNames:   "Updated LastNames")
+
+    assert_equal "Updated FirstNames",  user.first_names
+    assert_equal "Updated MiddleNames", user.middle_names
+    assert_equal "Updated LastNames",   user.last_names
+  end
 end
