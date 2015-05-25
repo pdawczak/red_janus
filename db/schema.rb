745b78af12e143a9e64848c043910d2b4732f4a2
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-05-25 11:03:06 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,32 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523144333) do
+ActiveRecord::Schema.define(version: 20150524195821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                             default: "", null: false
+    t.string   "encrypted_password",                default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                     default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title",                               null: false
-    t.string   "first_names",                         null: false
+    t.string   "title",                  limit: 8,               null: false
+    t.string   "first_names",                                    null: false
     t.string   "middle_names"
-    t.string   "last_names",                          null: false
-    t.date     "dob",                                 null: false
+    t.string   "last_names",                                     null: false
+    t.date     "dob",                                            null: false
+    t.string   "username",               limit: 10,              null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
