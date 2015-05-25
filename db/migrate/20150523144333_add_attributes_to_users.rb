ba16dd3bc1f4aab83e7bd478994864fc9f9ca282
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-05-25 11:02:38 +0100

    Change columns length

--- db/migrate/20150523144333_add_attributes_to_users.rb
+++ db/migrate/20150523144333_add_attributes_to_users.rb
@@ -1,6 +1,6 @@
 class AddAttributesToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :title,        :string, null: false
+    add_column :users, :title,        :string, null: false, limit: 8
     add_column :users, :first_names,  :string, null: false
     add_column :users, :middle_names, :string
     add_column :users, :last_names,   :string, null: false
