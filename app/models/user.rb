e8ba19ff040ee08676de51c1f2ea3c4a48842410
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-05-25 11:03:29 +0100

    Implement username generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  using Utils::LegacyizeHash
   include Utils::LegacyActiveRecord
 
   rewrite plain_password: :password
@@ -8,4 +7,30 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_username
+
+  def full_name
+    "#{first_names} #{middle_names} #{last_names}".squeeze
+  end
+
+  def initials
+    full_name
+      .parameterize(" ")
+      .split
+      .map(&:first)
+      .join
+  end
+
+  private
+
+    def set_username
+      num = User.where("username LIKE ?", "#{initials}%").count
+
+      begin
+        username = "#{initials}#{num += 1}"
+      end while User.where(username: username).exists?
+
+      self.username = username
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,9 +19,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_legacy_new
-    user = User.new(firstNames: "Joe",
+    user = User.new(firstNames:  "Joe",
                     middleNames: "von",
-                    lastNames: "Tester")
+                    lastNames:   "Tester")
 
     assert_equal "Joe",    user.first_names
     assert_equal "von",    user.middle_names
@@ -36,5 +36,35 @@ class UserTest < ActiveSupport::TestCase
     assert_equal "Updated FirstNames",  user.first_names
     assert_equal "Updated MiddleNames", user.middle_names
     assert_equal "Updated LastNames",   user.last_names
+  end
+
+  def test_initials
+    user = User.new(first_names:  "Joe",
+                    middle_names: "von",
+                    last_names:   "Test")
+    assert_equal "jvt", user.initials
+
+    user = User.new(first_names: "Joe",
+                    last_names:  "Test")
+    assert_equal "jt", user.initials
+  end
+
+  def test_set_username
+    user_attributes = { email:        "test_example@tesgint.com",
+                        title:        "Mr",
+                        first_names:  "Joe",
+                        middle_names: "von",
+                        last_names:   "Testinger",
+                        password:     "supertestpass",
+                        dob:          Date.parse("1967-09-01") }
+
+    user = User.create(user_attributes)
+    assert_equal "jvt2", user.username
+
+    user_attributes.merge!(email: "another_email@test.com", 
+                           username: "zxc123")
+
+    user = User.create(user_attributes)
+    assert_equal "jvt3", user.username
   end
 end
